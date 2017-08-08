4f8a8803bef80bd4401bce8955acebf4e65af578
Jason Rouse <jayrand@jasons-air.attlocal.net>
2017-08-07 20:30:56 -0700

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,5 +1,17 @@
-var cardOne = "king";
-var cardTwo = "king";
-var cardThree = "queen";
-var cardFour = "queen";
-console.log("User flipped " + cardOne);
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
+var cardOne = cards[0];
+var cardTwo = cards[1];
+
+cardsInPlay.push(cardOne);
+cardsInPlay.push(cardTwo);
+
+if (cardsInPlay.length === 2) {
+	if (cardOne === cardTwo) {
+		alert("You found a match!");
+	} else {
+		alert("Sorry, try again.");
+	}
+}
+
+console.log("User flipped " + cardsInPlay);
