1aefdb7bf5a4cac40c401bd4618975381797f402
Jason Rouse <jayrand@Jasons-MacBook-Air.local>
2017-08-08 12:48:35 -0700

    created gme logic

--- js/main.js
+++ js/main.js
@@ -1,17 +1,18 @@
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
-var cardOne = cards[0];
-var cardTwo = cards[1];
-
-cardsInPlay.push(cardOne);
-cardsInPlay.push(cardTwo);
-
-if (cardsInPlay.length === 2) {
-	if (cardOne === cardTwo) {
+var checkForMatch = function() {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
 		alert("You found a match!");
 	} else {
 		alert("Sorry, try again.");
 	}
 }
 
-console.log("User flipped " + cardsInPlay);
+var flipCard = function(cardId) {
+	checkForMatch();
+	console.log("User flipped " + cards[cardId]);
+	cardsInPlay.push(cards[cardId]);	
+}
+
+flipCard(0);
+flipCard(2);
